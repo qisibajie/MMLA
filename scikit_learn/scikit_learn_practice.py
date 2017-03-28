cb4f5bf6636594898e3c05642c0d75dbafc8113a
JeromeLu <arvinblureple@163.com>
2017-03-28 20:06:51 +0800

    add data transform func

--- scikit_learn/scikit_learn_practice.py
+++ scikit_learn/scikit_learn_practice.py
@@ -1,20 +1,42 @@
 from sklearn.feature_extraction import DictVectorizer
+from functools import reduce
+import pandas as pd
+import numpy as np
 
-def test_dict_vectorizer():
-    data = [{'BKG': 'AAM'}, {'BKG': 'ALM'}, {'BKG': 'LLK'}, {'TLD': 'AAA'}]
+def test_dict_vectorizer(data):
+    # data = [{'BKG': 'AAM'}, {'BKG': 'ALM'}, {'BKG': 'LLK'}, {'TLD': 'AAA'}]
     vec = DictVectorizer()
     vec.fit(data)
     # matrix = vec.fit_transform(data)
-    t = vec.transform([{'BKG': 'AAM'}])
-    print('this is t: ')
-    print(t.toarray())
+    # t = vec.transform([{'BKG': 'AAM'}])
+    # print('this is t: ')
+    # print(t.toarray())
     # print(matrix)
     # a = matrix.toarray()
     # print(a)
-    names = vec.get_feature_names()
-    print(names)
+    # names = vec.get_feature_names()
+    # print(names)
     # map = vec.inverse_transform(matrix)
-    print(map)
+    # print(map)
+    return vec
 
+def data_transform():
+    df = pd.read_csv('../data_set/test20170314060000.csv')
+    column_value = df.iloc[0:, 1]
+    column_value_list = column_value.tolist()
+    func = lambda x, y: x if y in x else x + [y]
+    distinct_column_value_list = reduce(func, [[], ] + column_value_list)
 
-test_dict_vectorizer()
+    training_data = []
+    for el in distinct_column_value_list:
+        r = {column_value.name: str(el)}
+        training_data.append(r)
+    print(training_data)
+    return training_data
+
+if __name__ == "__main__":
+    data = data_transform()
+    vec = test_dict_vectorizer(data)
+    t = vec.transform({'releaseOffice': 'KHI'})
+    print(t)
+    print(vec.get_feature_names())
